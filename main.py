dcc5a8a95139e9f6701f294b190444f38cce3951
MikhaelVinicius <mickaelsoel@gmail.com>
2023-01-21 05:11:23 -0300

    Só testando umas coisas

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-#Calculadora em python
+import math
 
+
+def main():
+
+    number = int(input())
+    operador = str(input())
+
+    def soma():
+        x = 0
+        for x in soma():
+            ++x
+
+            if x != 0:
+                print(number+number)
+            else:
+                number + 0
+                main()
+
+    if operador == "+":
+        soma()
+
+main()
